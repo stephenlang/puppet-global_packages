8445c0af67fa8af9e02ab60cacb92df6894e9725
Stephen Lang <slang@Stephens-MacBook-Air.local>
2014-01-05 04:09:57 -0500

    Initial commit

---
+++ manifests/params.pp
@@ -0,0 +1,16 @@
+# params.pp
+
+class global_packages::params {
+
+  case $osfamily {
+    'RedHat': {
+      $package_list = ['git', 'screen', 'wget', 'curl', 'mtr', 'rcs', 'mlocate']
+    }
+    'Debian': {
+      $package_list = ['git', 'screen', 'wget', 'curl', 'mtr', 'rcs', 'mlocate']
+    } 
+    default: {
+      fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem}, module ${module_name} only supports osfamily: RedHat and Debian")
+    }
+  }
+}
