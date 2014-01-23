66d963aab90dd30924ded002674fede6df38786f
Stephen Lang <slang@Stephens-MacBook-Air.local>
2014-01-23 01:40:31 -0500

    Adding sysstat

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,10 +4,10 @@ class global_packages::params {
 
   case $osfamily {
     'RedHat': {
-      $package_list = ['git', 'screen', 'wget', 'curl', 'mtr', 'rcs', 'mlocate']
+      $package_list = ['sysstat', 'git', 'screen', 'wget', 'curl', 'mtr', 'rcs', 'mlocate']
     }
     'Debian': {
-      $package_list = ['git', 'screen', 'wget', 'curl', 'mtr', 'rcs', 'mlocate']
+      $package_list = ['sysstat', 'git', 'screen', 'wget', 'curl', 'mtr', 'rcs', 'mlocate']
     } 
     default: {
       fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem}, module ${module_name} only supports osfamily: RedHat and Debian")
